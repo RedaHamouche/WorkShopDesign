008dc6fbe6b8a1ee80ccf3da6cc78264ce9681ec
Reda Hamouche <reda.hamouche@outlook.com>
2022-02-06 19:54:42 +0100

    a bunch of things lol

---
+++ src/javascript/script.js
@@ -0,0 +1,21 @@
+const inputPassword = document.querySelector(".input[type=password]");
+
+const succesSvg = document.querySelector(".input[type=password] ~ span")
+
+
+let timer;              // Timer identifier
+const waitTime = 100;   
+
+inputPassword.addEventListener('keyup', (e) => {
+    // Clear timer
+    clearTimeout(timer);
+
+    // Wait for X ms and then process the request
+    timer = setTimeout(() => {
+        if(e.target.value === "password") {
+            succesSvg.classList.add("succes");
+        } else {
+            succesSvg.classList.remove("succes");
+        }
+    }, waitTime);
+});
